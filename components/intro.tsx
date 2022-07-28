923f9bc9b14f29ee2aa4f03b9859ee0f2c74cd49
moody-jnan <jithya.n@outlook.com>
2022-07-28 22:14:10 +1000

    Updated intro and title. Removed hero post

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,23 +1,22 @@
-import { CMS_NAME } from '../lib/constants'
+import { CMS_NAME } from "../lib/constants";
 
 const Intro = () => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <hgroup className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        A Dev Blog
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
+        A collection of thoughts on full stack web development by{" "}
         <a
-          href="https://nextjs.org/"
+          href="https://www.linkedin.com/in/jithyan/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
-      </h4>
-    </section>
-  )
-}
+          Jithya Nanayakkara
+        </a>
+      </h2>
+    </hgroup>
+  );
+};
 
-export default Intro
+export default Intro;
