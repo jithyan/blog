467677c1a1af3d4f486faf61d7f5b42df8ba7003
moody-jnan <jithya.n@outlook.com>
2022-07-31 11:44:13 +1000

    Move to single col blog

--- components/home/intro.tsx
+++ components/home/intro.tsx
@@ -2,20 +2,23 @@ import { CMS_NAME } from "../../lib/constants";
 
 const Intro = () => {
   return (
-    <hgroup className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <hgroup className="flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         A Dev Blog
       </h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A place to consolidate my thoughts on building full stack web apps in
-        React and Typescript. By{" "}
+        A place to consolidate my thoughts on building web apps in React and
+        Typescript.
+      </h2>
+      <h3>
+        By{" "}
         <a
           href="https://www.linkedin.com/in/jithyan/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
           Jithya Nanayakkara
         </a>
-      </h2>
+      </h3>
     </hgroup>
   );
 };
