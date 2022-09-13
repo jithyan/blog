b746237bfe765d4f766cf0041009124eb38c97e9
moody-jnan <jithya.n@outlook.com>
2022-09-13 20:35:18 +1000

    Fix markdown image links

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -3,6 +3,16 @@ import html from "remark-html";
 import matter from "gray-matter";
 
 export default function markdownToHtml(markdown: string) {
-  const result = matter(markdown);
-  return result.content;
+  let { content } = matter(markdown);
+
+  if (process.env.NODE_ENV === "production") {
+    const regex = /\!\[(.*?)\]\((.*?)\)/gm;
+    let matches;
+
+    while ((matches = regex.exec(content)) !== null) {
+      content = content.replace("](" + matches[2], `](/blog${matches[2]}`);
+    }
+  }
+
+  return content;
 }
